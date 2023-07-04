7dba058b2ebb24361ca9c486ae246746cb77078c
Macko <prvinsm21@gmail.com>
2023-07-05 01:34:19 +0530

    Files added

---
+++ src/main/java/com/gaming/gamingsite/GamingsiteApplication.java
@@ -0,0 +1,13 @@
+package com.gaming.gamingsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaming/gamingsite/GamingsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaming.gamingsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
