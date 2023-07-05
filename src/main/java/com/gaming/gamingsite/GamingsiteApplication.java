7a760ab402c807a4731c91ecb5ab81b2c85e4f42
Macko <prvinsm21@gmail.com>
2023-07-06 01:09:29 +0530

    Files added

--- src/main/java/com/gaming/gamingsite/GamingsiteApplication.java
+++ src/main/java/com/gaming/gamingsite/GamingsiteApplication.java
@@ -2,12 +2,20 @@ package com.gaming.gamingsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 @SpringBootApplication
 public class GamingsiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GamingsiteApplication.class, args);
 	}
+
+	@GetMapping("/")
+    public String home() {
+        return "index.html";
+    }
 
 }
